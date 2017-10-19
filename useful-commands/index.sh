a85f3e69fb7bb89c2bac567f27006272d66d5122
LiuYue <hangxingliu@gmail.com>
2017-10-20 02:40:25 +0800

    add journalctl and lsyncd

--- useful-commands/index.sh
+++ useful-commands/index.sh
@@ -263,3 +263,12 @@ split file -b 1k -d path/prefix.
 # 这个例子会生成文件 path/prefix.01 path/prefix.02 ...
 split file.txt -l 10 part.txt.
 # 这个例子会将文本文件分割成每10行一个文件
+
+# Journalctl 查看日志
+# 查看Nginx服务(u: unit) 从今天开始的日志
+journalctl -u nginx.service --since today
+-n [lineNumbers]: # -n all 全部行 默认值为10行
+-f # Follow 不断显示新的日志
+-r # 反转显示日志(先显示最新的日志) 
+-p [priority] # 根据日志优先级筛选日志
+# (0: 出现/1: 警告/2: 暴击/3: 错误/4: 警告/5: 通知/6: INFO/7: 调试)
---
+++ useful-commands/lsyncd.sh
@@ -0,0 +1,35 @@
+# lsyncd (Live Syncing Daemon) 实时同步
+# https://github.com/axkibe/lsyncd.git
+
+sudo apt install lsyncd # 截止 2017/10/20 最新版本: 2.2.2
+# 命令行用法参考 man lsyncd 或 lsyncd --help
+
+# lsyncd 配置文件 + 服务
+# 创建相关文件/文件夹
+sudo mkdir /var/log/lsyncd # lsyncd 日志文件所在
+sudo mkdir /etc/lsyncd
+sudo vim /etc/lsyncd/lsyncd.conf.lua
+# 配置内容: (https://axkibe.github.io/lsyncd/manual/config/file/)
+settings {
+	logfile = "/var/log/lsyncd/lsyncd.log",
+	statusFile = "/var/log/lsyncd/lsyncd.status"
+}
+sync {
+--  # rsync ssh mode
+	default.rsyncssh,
+--  # duration: 1 second; default value: 15 seconds
+	delay = 1,
+	exclude =  {"*.tmp", ".git"},
+	source = "source path",
+	host = "user@host", targetdir = "path in host",
+	rsync = {
+		archive = true, compress = false, whole_file = false,
+--		# UserKnownHostsFile: 避免 Host key verification failed 错误
+		rsh = "ssh -p 22 -i /home/user/.ssh/id_rsa -o UserKnownHostsFile=/home/user/.ssh/known_hosts"
+	}
+}
+sync {...} ...
+
+# 重启lsyncd服务
+sudo service lsyncd restart
+# 如果出现异常可以 journalctl 查看日志 或查看/var/log/lsyncd下的日志
