6ad94ac5a85f094b3bf14dd32721ffa986f0d664
Liu Yue <hangxingliu@gmail.com>
2018-07-21 22:04:55 +0800

    update file names

--- src/bash-completion.sh
+++ src/bash-completion.sh
@@ -17,8 +17,8 @@ _useful_commands_completion() {
 	OPTS="-a -A -b -B -f -F -m --mini --help --color --no-color";
 	FILES_ARRAY=( awk bash curl disk docker ffmpeg find_xargs firewall git gpg gui_software
                 httpie image_magick index inotify install_deluge_on_centos_6 install_macos_in_virtualbox
-                log lsyncd monitor_and_usage mysql proxy qt raspberrypi samba
-                sed ssh_enc ssh swap ui vagrant vim vm xbindkeys );
+                log lsyncd monitor_and_usage multimedia mysql proxy qt raspberrypi
+                samba sed ssh_enc ssh swap ui vagrant vim vm xbindkeys );
 	for FNAME in "${FILES_ARRAY[@]}"; do
 		FILES="${FILES} ${FNAME}";
 		_F_FILES="${_F_FILES} -f${FNAME}";
